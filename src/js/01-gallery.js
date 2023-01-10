2da275fd9d75051121d0818d108be182540bc6c6
Bohdan <nalivkin.bog@gmail.com>
2023-01-10 16:01:12 +0200

    final push

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,12 @@
-import { galleryItems } from "./gallery-items.js";
-// Change code below this line
-const galleryRefs = document.querySelector(".gallery");
-galleryRefs.addEventListener("click", onShowBigImage);
+import { galleryItems } from './gallery-items';
+console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryRefs = document.querySelector('.gallery');
+galleryRefs.addEventListener('click', onShowBigImage);
 
 (function createGalleryMarkup() {
   const elementCreateGallery = galleryItems
@@ -19,31 +24,19 @@ galleryRefs.addEventListener("click", onShowBigImage);
     </div>
     `;
     })
-    .join("");
-  galleryRefs.insertAdjacentHTML("beforeend", elementCreateGallery);
+
+    .join('');
+  galleryRefs.insertAdjacentHTML('beforeend', elementCreateGallery);
 })();
 
 function onShowBigImage(e) {
   e.preventDefault();
-  if (!e.target.classList.contains("gallery__image")) {
+  if (!e.target.classList.contains('gallery__image')) {
     return;
   }
-
-  let totalImageSrc = e.target.dataset.source;
-
-  const modal = basicLightbox.create(
-    `<img src="${totalImageSrc}" width="900" height="700">`
-  );
-  modal.show();
-
-  if (modal.visible()) {
-    window.addEventListener("keydown", onPressKeyESC);
-  }
-
-  function onPressKeyESC(cls) {
-    if (cls.code === "Escape") {
-      modal.close();
-      window.removeEventListener("keydown", onPressKeyESC);
-    }
-  }
 }
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
---
+++ src/js/03-feedback.js
@@ -0,0 +1,35 @@
+var throttle = require('lodash.throttle');
+
+const form = document.querySelector('.feedback-form');
+const state = JSON.parse(localStorage.getItem('feedback-form-state'));
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
+
+form.addEventListener('input', throttle(onFormData, 500));
+form.addEventListener('submit', onSubmitForm);
+
+const formData = {};
+
+function onFormData(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function onSubmitForm(e) {
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  e.preventDefault();
+  if (e.target.email.value === '' || e.target.message.value === '') {
+    alert('Заповніть поле');
+    return;
+  }
+
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+(function dataFromLocalStorage() {
+  if (state) {
+    email.value = state.email;
+    message.value = state.message;
+  }
+})();
